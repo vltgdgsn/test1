d7f4100fd8b66c79d56a17a01cd5ea2028667c47
vltgdgsn <95680309+vltgdgsn@users.noreply.github.com>
2021-12-07 01:40:55 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+   <p>niftydoo</p>
       </main>
 
       <Footer />
